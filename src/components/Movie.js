a9f289d3de501d33d3576e7622c0c1de47acf7ad
samie820 <samuelomole70@gmail.com>
2018-11-27 23:15:52 +0100

    Convert Movie component to a functional component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,20 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 
-const DEFAULT_PLACEHOLDER_IMAGE = "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
+const DEFAULT_PLACEHOLDER_IMAGE =
+  "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
 
-class Movie extends Component {
-  render() {
-    const poster = this.props.meta.Poster === 'N/A' ? DEFAULT_PLACEHOLDER_IMAGE : this.props.meta.Poster
-    return (
-      <div className="movie">
-        <h2>{this.props.meta.Title}</h2>
-        <div>
-          <img width="200" alt={`The movie titled: ${this.props.meta.Title}`} src={poster} />
-        </div>
-        <p>({this.props.meta.Year})</p>
+const Movie = props => {
+  const poster =
+    props.meta.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : props.meta.Poster;
+  return (
+    <div className="movie">
+      <h2>{props.meta.Title}</h2>
+      <div>
+        <img
+          width="200"
+          alt={`The movie titled: ${props.meta.Title}`}
+          src={poster}
+        />
       </div>
-    );
-  }
-}
+      <p>({props.meta.Year})</p>
+    </div>
+  );
+};
 
 export default Movie;
