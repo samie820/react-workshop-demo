c483f504e287287d1be8e1cd599cddc5ca3710ab
samie820 <samuelomole70@gmail.com>
2018-11-27 21:48:35 +0100

    Add the movies array to the App.js component state and make the styling a bit responsive

--- src/components/App.js
+++ src/components/App.js
@@ -4,66 +4,94 @@ import Header from "./Header";
 import Movie from "./Movie";
 
 /*
-    ------ PROPS -------
-    When it comes to passing string props, there are many ways to do it, but for the sake if this workshop we will
-    Only work on two methods
+  ------- STATE -------
+  What is state in a React app? You can think of it as a single JavaScript object which represents all the data in your app.
+  State can be defined on any component,
+  but if you want to share state between components then it's better to define it on the top-level component.
+  State can then be passed down to child components and accessed as required.
 
-    METHOD 1: (Directly)
-        e.g. <Header text="direct string" />
-    
-    METHOD 2: (Using a variable)
-        render(){
-            const headerText = "Header String";
-            return (
-                <Header text={headerText} />
-            )
-        }
-    
-    For number props we can pass it as:
-        e.g <Header numberProps={10} />
-    
-    For boolean props we can pass it like this:
-        e.g. <Header booleanProps={true} />
-            OR
-            <Header booleanProps={false} />
-    
-    For object props we can pass them like this:
-        e.g. <Header objectProps={{
-            key: value
-        }}
+  NOTE: For now state can be created like the example below:
+
+  constructor(){
+    super();
+    this.state = {};
+  }
 */
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [
+        {
+          title: "Ferris Bueller's Day Off",
+          year: "1986",
+          description:
+            "A high school wise guy is determined to have a day off from school, despite what the principal thinks of that.",
+          poster:
+            "https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhYjQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg"
+        },
+        {
+          title: "Bridget Jones' Diary",
+          year: "2001",
+          description:
+            "A British woman is determined to improve herself while she looks for love in a year in which she keeps a personal diary.",
+          poster:
+            "https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhYjQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg"
+        },
+        {
+          title: "Bridget Jones' Diary",
+          year: "2001",
+          description:
+            "A British woman is determined to improve herself while she looks for love in a year in which she keeps a personal diary.",
+          poster:
+            "https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhYjQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg"
+        },
+        {
+          title: "Bridget Jones' Diary",
+          year: "2001",
+          description:
+            "A British woman is determined to improve herself while she looks for love in a year in which she keeps a personal diary.",
+          poster:
+            "https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhYjQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg"
+        },
+        {
+          title: "Bridget Jones' Diary",
+          year: "2001",
+          description:
+            "A British woman is determined to improve herself while she looks for love in a year in which she keeps a personal diary.",
+          poster:
+            "https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhYjQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg"
+        },
+        {
+          title: "50 First Dates",
+          year: "2004",
+          description:
+            "Henry Roth is a man afraid of commitment up until he meets the beautiful Lucy. They hit it off and Henry think he's finally found the girl of his dreams.",
+          poster:
+            "https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhYjQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg"
+        },
+        {
+          title: "Matilda",
+          year: "1996",
+          description:
+            "Story of a wonderful little girl, who happens to be a genius, and her wonderful teacher vs. the worst parents ever and the worst school principal imaginable.",
+          poster:
+            "https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhYjQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg"
+        }
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <Header text="Samuel's Movie App" />
         <p className="App-intro">Sharing a few of our favourite movies</p>
         <div className="movies">
-          <Movie
-            title="50 First Dates"
-            year="2004"
-            description="Henry Roth is a man afraid of commitment up until he meets the beautiful Lucy. They hit it off and Henry think he's finally found the girl of his dreams."
-            poster="https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhYjQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg"
-          />
-          <Movie
-            title="Ferris Bueller's Day Off"
-            year="1986"
-            description="A high school wise guy is determined to have a day off from school, despite what the principal thinks of that."
-            poster="https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhYjQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg"
-          />
-          <Movie
-            title="Matilda"
-            year="1996"
-            description="Story of a wonderful little girl, who happens to be a genius, and her wonderful teacher vs. the worst parents ever and the worst school principal imaginable."
-            poster="https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhYjQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg"
-          />
-          <Movie
-            title="Dirty Dancing"
-            year="1987"
-            description="Spending the summer at a Catskills resort with her family, Frances 'Baby' Houseman falls in love with the camp's dance instructor, Johnny Castle."
-            poster="https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhYjQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg"
-          />
+          {this.state.movies.map((movie, index) => (
+            <Movie key={`${index}-${movie.title}`} meta={movie} />
+          ))}
         </div>
       </div>
     );
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,12 +4,12 @@ class Movie extends Component {
   render() {
     return (
       <div className="movie">
-        <h2>{this.props.title}</h2>
+        <h2>{this.props.meta.title}</h2>
         <div>
-          <img width="200" alt={`The movie titled: ${this.props.title}`} src={this.props.poster} />
+          <img width="200" alt={`The movie titled: ${this.props.meta.title}`} src={this.props.meta.poster} />
         </div>
-        <p>({this.props.year})</p>
-        <p>{this.props.description}</p>
+        <p>({this.props.meta.year})</p>
+        <p>{this.props.meta.description}</p>
       </div>
     );
   }
